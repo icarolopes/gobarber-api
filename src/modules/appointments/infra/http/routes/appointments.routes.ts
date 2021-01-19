a44e90210d14ecca893fd0dca6574ab363d59290
Icaro Lopes <icarofernandeslopes@gmail.com>
2021-01-18 22:24:21 -0300

    init - 11 - Usando Controllers

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { parseISO } from 'date-fns'
-import { container } from 'tsyringe'
 
-import { CreateAppointmentService } from '@modules/appointments/services/CreateAppointmentService'
 import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated'
+import { AppointmentsController } from '../controllers/AppointmentsController'
 
-export const appointmentsRouter = Router()
+const appointmentsController = new AppointmentsController()
+
+const appointmentsRouter = Router()
 
 appointmentsRouter.use(ensureAuthenticated)
 
@@ -14,17 +14,6 @@ appointmentsRouter.use(ensureAuthenticated)
 //   return response.json(appointments)
 // })
 
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body
+appointmentsRouter.post('/', appointmentsController.create)
 
-  const parsedDate = parseISO(date)
-
-  const createAppointment = container.resolve(CreateAppointmentService)
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id
-  })
-
-  return response.json(appointment)
-})
+export { appointmentsRouter }
--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,21 +1,11 @@
 import { Router } from 'express'
-import { container } from 'tsyringe'
 
-import { AuthenticateUserService } from '@modules/users/services/AuthenticateUserService'
+import { SessionsController } from '../controllers/SessionsController'
+
+const sessionsControlleer = new SessionsController()
 
 const sessionsRouter = Router()
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body
-
-  const authenticateUser = container.resolve(AuthenticateUserService)
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password
-  })
-
-  return response.json({ user, token })
-})
+sessionsRouter.post('/', sessionsControlleer.create)
 
 export { sessionsRouter }
